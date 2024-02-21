4b2a0f38af7632d21f01a04e3d22bacca84ca357
MMirza11 <k2102926@kingston.ac.uk>
2024-02-21 19:16:24 +0000

    Squashed commit of the following:
    commit d854bef6fb8f7ac39987509a9ae5588ab3c348d8
    Author: MMirza11 <k2102926@kingston.ac.uk>
    Date:   Wed Feb 21 18:56:49 2024 +0000
    
        made the drawer navigation
    
        make the files for the users.
        added the data for the user crudler
    
    commit bf973391e457077592a1bbd43cf70a625fcb680b
    Author: MMirza11 <k2102926@kingston.ac.uk>
    Date:   Mon Feb 12 19:36:29 2024 +0000
    
        modify bit
    
    commit 6034058f3f66f841af9c8ecb6c7b6dad2ca0e02a
    Author: MMirza11 <k2102926@kingston.ac.uk>
    Date:   Thu Feb 8 15:49:57 2024 +0000
    
        Fixed the activity 11 Icons.Close error
    
    commit f7fa9b9dc5a8e1f7553a8cf3fadc3a5410e70932
    Author: MMirza11 <k2102926@kingston.ac.uk>
    Date:   Wed Feb 7 22:44:12 2024 +0000
    
        Completed Activity 10

---
+++ src/components/screens/Module/ModuleListScreen.js
@@ -0,0 +1,64 @@
+import { LogBox, StyleSheet } from "react-native";
+import Screen from "../../layout/Screen.js";
+import initialModules from "../../../data/modules.js";
+import ModuleList from "../../entity/modules/ModuleList.js";
+import { useState } from "react";
+import Icons from "../../UI/Icons.js";
+import { Button, ButtonTray } from "../../UI/Button.js";
+import RenderCount from "../../UI/RenderCount.js";
+
+const ModuleListScreen = ({ navigation }) => {
+  // Initialisations -----
+  LogBox.ignoreLogs([
+    "Non-serializable values were found in the navigation state",
+  ]);
+
+  // State ---------------
+  const [modules, setModules] = useState(initialModules);
+  // Handlers ------------
+  const handleDelete = (module) => {
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+    console.log(`Module ${module.ModuleCode} deleted`);
+  };
+
+  const handleAdd = (module) => setModules([...modules, module]);
+
+  const handleModify = (updatedModule) =>
+    setModules(
+      modules.map((module) =>
+        module.ModuleID === updatedModule.ModuleID ? updatedModule : module
+      )
+    );
+
+  const onDelete = (module) => {
+    handleDelete(module);
+    navigation.goBack();
+  };
+
+  const onAdd = (module) => {
+    handleAdd(module);
+    navigation.goBack();
+  };
+  const onModify = (module) => {
+    handleModify(module);
+    navigation.navigate("ModuleListScreen");
+  };
+
+  const goToViewScreen = (module) =>
+    navigation.navigate("ModuleViewScreen", { module, onDelete, onModify });
+  const goToAddScreen = () => navigation.navigate("ModuleAddScreen", { onAdd });
+
+  // View ----------------  can put after "<Screen>" <RenderCount  />
+  return (
+    <Screen>
+      <ButtonTray>
+        <Button label="Add" icon={<Icons.Add />} onClick={goToAddScreen} />
+      </ButtonTray>
+      <ModuleList modules={modules} onSelect={goToViewScreen} />
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleListScreen;
---
+++ src/components/screens/User/UserViewScreen.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+import Screen from "../../layout/Screen";
+import ModuleView from "../../entity/modules/ModuleView";
+
+const ModuleViewScreen = ({ navigation, route }) => {
+  // Initialisations -----
+  const { module, onDelete, onModify } = route.params;
+
+  // State ---------------
+  // Handlers ------------
+  const goToModifyScreen = () =>
+    navigation.navigate("ModuleModifyScreen", { module, onModify });
+  // View ----------------
+  return (
+    <Screen>
+      <ModuleView
+        module={module}
+        onDelete={onDelete}
+        onModify={goToModifyScreen}
+      />
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleViewScreen;
